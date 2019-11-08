fc4e23d1958084dd7a54d0e8d34d03b3bc38b14e
lisaw97 <lisawen0614@gmail.com>
2019-11-07 18:25:30 -0800

    Reset database

--- db/migrate/20191107230509_create_stocks.rb
+++ db/migrate/20191107230509_create_stocks.rb
@@ -2,10 +2,10 @@ class CreateStocks < ActiveRecord::Migration[5.1]
   def change
     create_table :stocks do |t|
       t.string :company_name, null: false
-      t.string :symbol, null: false
+      t.string :ticker, null: false
       t.timestamps
     end
 
-    add_index :stocks, :symbol, unique: true
+    add_index :stocks, :ticker, unique: true
   end
 end
